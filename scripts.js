008d5c52cb0090024d5de5fd015ee3c0e7362ba7
Pat Kelly <PatK80@gmail.com>
2024-09-08 11:00:42 -0400

    Adding JS for copyright year

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// Variables //
+let copyrightDate    = document.querySelector("#copyright-date");
 const toggleButton  = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks   = document.getElementsByClassName("navbar-links")[0];
 const hamburgerIcon = document.querySelector("#hamburger-icon");
@@ -8,6 +10,7 @@ const about         = document.querySelector("#about-link");
 const projects      = document.querySelector("#projects-link");
 const contact       = document.querySelector("#contact-link");
 
+// Mobile Hamburger Menu //
 toggleButton.addEventListener("click", () => {
   switchToggleStatus();
 });
@@ -30,3 +33,6 @@ function switchToggleStatus() {
   bar2.classList.toggle("bar-2-active");
   bar3.classList.toggle("bar-3-active");
 }
+
+// Update Copyright Date with current year //
+copyrightDate.innerText = new Date().getFullYear();
