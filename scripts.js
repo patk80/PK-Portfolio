bf425bbd5df29b96a1732a26d6b1f194d1de3802
Pat Kelly <PatK80@gmail.com>
2024-12-05 18:26:28 -0500

    Updating JS file to fix date not working, removing old things

--- scripts.js
+++ scripts.js
@@ -1,38 +1,5 @@
 // Variables //
-let copyrightDate    = document.querySelector("#copyright-date");
-const toggleButton  = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks   = document.getElementsByClassName("navbar-links")[0];
-const hamburgerIcon = document.querySelector("#hamburger-icon");
-const bar1          = document.querySelector("#bar-1");
-const bar2          = document.querySelector("#bar-2");
-const bar3          = document.querySelector("#bar-3");
-const about         = document.querySelector("#about-link");
-const projects      = document.querySelector("#projects-link");
-const contact       = document.querySelector("#contact-link");
-
-// Mobile Hamburger Menu //
-toggleButton.addEventListener("click", () => {
-  switchToggleStatus();
-});
-
-about.addEventListener("click", () => {
-  switchToggleStatus();
-});
-
-projects.addEventListener("click", () => {
-  switchToggleStatus();
-});
-
-contact.addEventListener("click", () => {
-    switchToggleStatus();
-});
-
-function switchToggleStatus() {
-  navbarLinks.classList.toggle("active");
-  bar1.classList.toggle("bar-1-active");
-  bar2.classList.toggle("bar-2-active");
-  bar3.classList.toggle("bar-3-active");
-}
+let copyrightDate   = document.querySelector("#copyright-date");
 
 // Update Copyright Date with current year //
 copyrightDate.innerText = new Date().getFullYear();
